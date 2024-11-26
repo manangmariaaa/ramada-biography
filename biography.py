7db00e68011acff725143f51d4bc8e302d3e5dd6
manangmariaaa <ramadamarialyn@gmail.com>
2024-11-26 14:02:22 +0800

    Add files via upload

--- biography.py
+++ biography.py
@@ -4,15 +4,15 @@ import datetime as datetime
 
 st.title("My Biography")
 
-st.sidebar.header("Full Name")
-name = st.sidebar.text_input("Name", "Marialyn Ramada")
-age = st.sidebar.number_input("Age", min_value=0, max_value=120, step=1)
-occupation = st.sidebar.text_input("Occupation", "")
-hobbies = st.sidebar.text_area("Hobbies (comma-separated)", "")
-goals = st.sidebar.text_area("Goals or Aspirations", "")
+st.header("Full Name")
+name = st.ext_input("Name", "Marialyn Ramada")
+age = st.number_input("Age", min_value=0, max_value=120, step=1)
+occupation = st.text_input("Occupation", "")
+hobbies = st.text_area("Hobbies (comma-separated)", "")
+goals = st.text_area("Goals or Aspirations", "")
 
 # Display biography
-if st.sidebar.button("Generate Biography"):
+if st.button("Generate Biography"):
     if name and age and occupation:
         st.header("Your Biography")
         st.write(f"*Name:* {name}")
